9b3ee77113b83c89cef540a1bedf6c744f4f47c5
Mauricio Arismendi <arismendimauricio@gmail.com>
2019-12-09 15:40:17 -0300

    Web app and route react setup

---
+++ src/components/about/AboutPage.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const AboutPage = () => (
+  <div>
+    <h2>Acerca</h2>
+    <p>
+      Esta aplicación utiliza React, Redux, React Router y otras librerias
+      útiles.
+    </p>
+  </div>
+);
+
+export default AboutPage;
