3f767707a1a10578df7d07bfdaaddeedf435919a
Mauricio Arismendi <arismendimauricio@gmail.com>
2019-12-10 14:37:56 -0300

    API Mock and use of redux thunks

--- src/redux/configureStore.js
+++ src/redux/configureStore.js
@@ -1,16 +1,14 @@
-import {
-  createStore,
-  applyMiddleware,
-  compose
-} from "redux";
+import { createStore, applyMiddleware, compose } from "redux";
 import rootReducer from "./reducers";
 import reduxInmutableStateInvariant from "redux-immutable-state-invariant";
+import thunk from "redux-thunk";
 
 export default function configureStore(initialState) {
-  const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose; // add suppor for Redux dev tools
+  const composeEnhancers =
+    window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose; // add suppor for Redux dev tools
   return createStore(
     rootReducer,
     initialState,
-    composeEnhancers(applyMiddleware(reduxInmutableStateInvariant()))
+    composeEnhancers(applyMiddleware(thunk, reduxInmutableStateInvariant()))
   );
 }
